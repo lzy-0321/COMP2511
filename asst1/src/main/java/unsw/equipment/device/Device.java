f2a0c7790948168e703b288d1c2957590bd12e9b
Ziyao <z5340468@ad.unse.edu.au>
2023-06-28 02:31:37 +1000

    finished

--- asst1/src/main/java/unsw/equipment/device/Device.java
+++ asst1/src/main/java/unsw/equipment/device/Device.java
@@ -87,12 +87,10 @@ public abstract class Device implements EntityInfo, Sender, Receiver {
         fileHandler.setFile(file);
     }
 
-    @Override
     public List<File> getFileList() {
         return fileHandler.getFileList();
     }
 
-    @Override
     public EntityInfoResponse getEntityInfo() {
         if (fileHandler.isFileListEmpty()) {
             return new EntityInfoResponse(this.deviceId, this.position, this.height, this.type);
@@ -100,92 +98,74 @@ public abstract class Device implements EntityInfo, Sender, Receiver {
         return new EntityInfoResponse(this.deviceId, this.position, this.height, this.type, getFileInfoMap());
     }
 
-    @Override
     public Map<String, FileInfoResponse> getFileInfoMap() {
         return fileHandler.getFileInfoMap();
     }
 
-    @Override
     public Map<File, String> getSendSchedule() {
         return fileHandler.getSendSchedule();
     }
 
-    @Override
     public boolean isFileInList(File file, List<File> list) {
         return fileHandler.isFileInList(file, list);
     }
 
-    @Override
     public boolean isFileSending(File file) {
         return fileHandler.isFileSending(file);
     }
 
-    @Override
     public boolean isFileComplete(File file) {
         return fileHandler.isFileComplete(file);
     }
 
-    @Override
     public boolean isFileCompleteInThisTransfering(File file, int index, int size) {
         return fileHandler.isFileCompleteInThisTransfering(file, index, size);
     }
 
-    @Override
     public void addFileInfo(File file) {
         fileHandler.addFileInfo(file);
     }
 
-    @Override
     public void sendingFile(File file, boolean isComplete) {
         fileHandler.sendingFile(file, isComplete);
     }
 
-    @Override
     public void stopSendingFile(File file) {
         fileHandler.stopSendingFile(file);
     }
 
-    @Override
     public void receivingFile(File file, boolean isComplete) {
         fileHandler.receivingFile(file, isComplete);
     }
 
-    @Override
     public void stopReceivingFile(File file) {
         fileHandler.stopReceivingFile(file);
     }
 
-    @Override
     public void addFileToSendSchedule(File file, String id) {
         fileHandler.addFileToSendSchedule(file, id);
     }
 
-    @Override
     public int getCurrentFileSize(File file) {
         return fileHandler.getCurrentFileSize(file);
     }
 
-    @Override
     public File getFile(String fileName) {
         return fileHandler.getFile(fileName);
     }
 
-    @Override
     public File setSendFile(File file, int index, int size) {
         return fileHandler.setSendFile(file, index, size);
     }
 
-    @Override
     public File setSendFile(File file, int index) {
         return fileHandler.setSendFile(file, index);
     }
 
-    @Override
     public File setSendFileWithoutT(File file, int index) {
         return fileHandler.setSendFileWithoutT(file, index);
     }
 
-    @Override
     public void removeTLetter(File file) {
         fileHandler.removeTLetter(file);
     }
--- asst1/src/main/java/unsw/equipment/satellite/SatelliteHandlesFiles.java
+++ asst1/src/main/java/unsw/equipment/satellite/SatelliteHandlesFiles.java
@@ -22,7 +22,6 @@ public abstract class SatelliteHandlesFiles extends Satellite implements EntityI
         return fileHandler;
     }
 
-    @Override
     public List<File> getFileList() {
         return fileHandler.getFileList();
     }
@@ -34,92 +33,74 @@ public abstract class SatelliteHandlesFiles extends Satellite implements EntityI
         return new EntityInfoResponse(getSatelliteId(), getPosition(), getHeight(), getType(), getFileInfoMap());
     }
 
-    @Override
     public Map<String, FileInfoResponse> getFileInfoMap() {
         return fileHandler.getFileInfoMap();
     }
 
-    @Override
     public Map<File, String> getSendSchedule() {
         return fileHandler.getSendSchedule();
     }
 
-    @Override
     public boolean isFileInList(File file, List<File> list) {
         return fileHandler.isFileInList(file, list);
     }
 
-    @Override
     public boolean isFileSending(File file) {
         return fileHandler.isFileSending(file);
     }
 
-    @Override
     public boolean isFileComplete(File file) {
         return fileHandler.isFileComplete(file);
     }
 
-    @Override
     public boolean isFileCompleteInThisTransfering(File file, int index, int size) {
         return fileHandler.isFileCompleteInThisTransfering(file, index, size);
     }
 
-    @Override
     public void addFileInfo(File file) {
         fileHandler.addFileInfo(file);
     }
 
-    @Override
     public void sendingFile(File file, boolean isComplete) {
         fileHandler.sendingFile(file, isComplete);
     }
 
-    @Override
     public void stopSendingFile(File file) {
         fileHandler.stopSendingFile(file);
     }
 
-    @Override
     public void receivingFile(File file, boolean isComplete) {
         fileHandler.receivingFile(file, isComplete);
     }
 
-    @Override
     public void stopReceivingFile(File file) {
         fileHandler.stopReceivingFile(file);
     }
 
-    @Override
     public void addFileToSendSchedule(File file, String id) {
         fileHandler.addFileToSendSchedule(file, id);
     }
 
-    @Override
     public int getCurrentFileSize(File file) {
         return fileHandler.getCurrentFileSize(file);
     }
 
-    @Override
     public File getFile(String fileName) {
         return fileHandler.getFile(fileName);
     }
 
-    @Override
     public File setSendFile(File file, int index, int size) {
         return fileHandler.setSendFile(file, index, size);
     }
 
-    @Override
     public File setSendFile(File file, int index) {
         return fileHandler.setSendFile(file, index);
     }
 
-    @Override
     public File setSendFileWithoutT(File file, int index) {
         return fileHandler.setSendFileWithoutT(file, index);
     }
 
-    @Override
     public void removeTLetter(File file) {
         fileHandler.removeTLetter(file);
     }
